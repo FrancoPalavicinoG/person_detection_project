ebb8547d2f598e00e1c2ace9690355a03c847ffb
FrancoPalavicinoG <fppalavicino@miuandes.cl>
2024-06-20 00:04:10 -0400

    first commit

---
+++ build/esp-idf/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
@@ -0,0 +1,1 @@
+/Users/franco_palavicino/esp/idf/esp-idf/components/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
---
+++ build/esp-idf/mbedtls/mbedtls/library/version_features.c
@@ -0,0 +1,1 @@
+/Users/franco_palavicino/esp/idf/esp-idf/components/mbedtls/mbedtls/library/version_features.c
